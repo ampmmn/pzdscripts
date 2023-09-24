ee1c9a0efbcb385b17ee85e71b269da6b6abd608
ampmmn <htmnymgw@gmail.com>
2023-09-24 14:29:43 +0900

    追加

---
+++ loop-kyokuren.py
@@ -0,0 +1,147 @@
+# code:utf-8
+import pzd
+
+# loop-kyokuren.py
+# 極練周回
+#  2023/09/23
+# スクリプト開始時の前提: 
+#  - ノーマルダンジョン選択画面を表示していること
+#  - 「獄連の闘技場」が一番上に見えていること(獄連,極連ともに解放済であること)
+#  - 経験値バッジ
+# メンバ構成:
+#	L:サレーネ + スキブアシスト
+#     シヴィニア + スキブアシスト
+#     シヴィニア + スキブアシスト
+#	  木ゼローグ
+#	  万極 + フェイタン装備
+#	F:サレーネ
+# 1周の目安時間: 3分20秒程度(1時間に18-19周程度)
+#
+# メモ:
+#   - 初手でシヴィニアx2変身、木ゼローグの10ターン落ちコンなし→サレーネでヘイスト+2
+#   - 1Fから最後まで、2体のシヴィニアx2のスキルを交互に使ってずらす、で進む
+#   - 万極は火力要員
+#   - 落ちコンなしの効果が切れるので途中(9F)で再度木ゼローグのスキルを使用している
+#   - 完全ノーパズル+落ちコンなし編成なので画面状態で判断せずに単純なsleepによる待ちで処理している
+#     - とはいえコンボが多く発生した場合に、見切り発車によるずれが生じる可能性あり
+#   - 04:00の「日付が変わりました」スキップは実装してないので4時で流れが止まる一方、
+#     このスクリプト自体は実行し続けるため、誤操作の恐れあり(というかした)
+#
+#   - 現在使用している機種している機種を前提としてタップ/スワイプする座標値をハードコードしているのでどうにかする
+
+class loop_kyokuren:
+
+    def __init__(self):
+        self.pzd = pzd.pzd()
+        pass
+
+    # メンバのスキルをポチって、ドロップをずらす
+    def useSkillAndMove(self, index,*,msg=""):
+
+        pzd = self.pzd
+
+        # スキル使用
+        pzd.useSkill(index, msg=msg, wait=3)
+        # ずらして、次を待つ
+        pzd.swipe(120, 1410, 276, 1410, msg="ドロップをずらす", duration=230)
+        pzd.wait(9, msg="移動可能になるまで待機")
+    
+    # 1Fの処理
+    def battle1(self, pzd):
+        pzd.useSkill(1, msg="シヴィニア1変身", wait=3)
+        pzd.useSkill(2, msg="シヴィニア2変身", wait=3)
+        pzd.useSkill(3, msg="木ゼローグスキル使用", wait=2)
+        pzd.useSkill(0, msg="サレーネスキル使用", wait=3)
+        self.useSkillAndMove(1,msg="シヴィニア1スキル使用")
+    
+    # 2Fの処理
+    def battle2(self, pzd):
+        self.useSkillAndMove(2,msg="シヴィニア2スキル使用")
+    
+    # 3Fの処理
+    def battle3(self, pzd):
+        self.useSkillAndMove(1,msg="シヴィニア1スキル使用")
+    
+    # 4Fの処理
+    def battle4(self, pzd):
+        self.useSkillAndMove(2,msg="シヴィニア2スキル使用")
+    
+    # 5Fの処理
+    def battle5(self, pzd):
+        self.useSkillAndMove(1,msg="シヴィニア1スキル使用")
+    
+    # 6Fの処理
+    def battle6(self, pzd):
+        self.useSkillAndMove(2,msg="シヴィニア2スキル使用")
+    
+    # 7Fの処理
+    def battle7(self, pzd):
+        self.useSkillAndMove(1,msg="シヴィニア1スキル使用")
+    
+    # 8Fの処理
+    def battle8(self, pzd):
+        self.useSkillAndMove(2,msg="シヴィニア2スキル使用")
+    
+    # 9Fの処理
+    def battle9(self, pzd):
+        pzd.useSkill(3, msg="木ゼローグスキル使用", wait=2)
+        self.useSkillAndMove(1,msg="シヴィニア1スキル使用")
+    
+    # 10Fの処理
+    def battle10(self, pzd):
+        self.useSkillAndMove(2,msg="シヴィニア2スキル使用")
+    
+    # 11F(ボス)の処理
+    def battleBoss(self, pzd):
+        pzd.useSkill(1, msg="シヴィニア1スキル使用", wait=2.5)
+        # 最終フロアなので次のフロアの表示待ちは不要
+        pzd.swipe(120, 1410, 276, 1410, msg="ずらす", duration=230)
+    
+    # 周回のメイン処理
+    def loop(self):
+
+        pzd = self.pzd
+    
+        # ダンジョン選択
+        # (2つ目を選択、画面上の一番上に獄練が見えている状態を想定)
+        pzd.selectDangeon1(1, msg="「極練の闘技場」を選択")
+    
+        while True:
+            pzd.selectDangeon2(0, msg="「風華の悪戯」を選択")
+    
+            # スタミナ不足していたら回復
+            pzd.kaifuku()
+    
+            # 助っ人選択
+            pzd.selectFriend(0)
+    
+            # 各フロアの処理
+            self.battle1(pzd)
+            self.battle2(pzd)
+            self.battle3(pzd)
+            self.battle4(pzd)
+            self.battle5(pzd)
+            self.battle6(pzd)
+            self.battle7(pzd)
+            self.battle8(pzd)
+            self.battle9(pzd)
+            self.battle10(pzd)
+            self.battleBoss(pzd)
+
+            # TIPS画面と結果画面のスキップ
+            pzd.skipTips()
+            pzd.skipResult()
+
+if __name__ == "__main__":
+    kyokuren = loop_kyokuren()
+
+    # adb.exeが見つからない場合は終了
+    if kyokuren.pzd.isAvailable() == False:
+        print(f"adb.exe does not exist. [{kyokuren.pzd.ADBPath()}]")
+        quit()
+
+    try:
+        kyokuren.loop()
+    except KeyboardInterrupt:
+        pass
+
---
+++ pzd.py
@@ -0,0 +1,150 @@
+# code:utf-8
+import os
+import subprocess
+import time
+
+class ADB:
+
+    def __init__(self):
+        # 前回のswipe時の移動時間
+        self.lastDuration = 100
+        pass
+
+    def path(self):
+        # 実行環境によりパスは異なるので適宜設定する
+        return r"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe"
+
+    # タップ処理
+    def tap(self, x, y, *, msg="", duration=200, wait=0):
+
+        if msg != "":
+            print(msg)
+
+        cmd = [ self.path(), "shell", "input", "swipe", f'{x}', f'{y}', f'{x}', f'{y}', f"{duration}" ]
+        subprocess.run(cmd)
+
+        if wait>0:
+            self.wait(wait)
+
+    # スワイプ処理
+    def swipe(self, x1, y1, x2, y2,*, msg="", duration = 0, wait=0):
+
+        if msg != "":
+            print(msg)
+
+        d = duration
+        if d == 0:
+            d = self.lastDuration
+
+        cmd = [ self.path(), "shell", "input", "swipe", f'{x1}', f'{y1}', f'{x2}', f'{y2}', f"{duration}" ]
+        subprocess.run(cmd)
+
+        self.lastDuration = d
+
+        if wait>0:
+            self.wait(wait)
+
+    # 待ち
+    def wait(self, sec, msg = ""):
+        if msg != "":
+            print(msg)
+
+        time.sleep(sec)
+
+    # 戻るキー押下
+    def backKey(self,*,msg = "", wait=0):
+
+        if msg != "":
+            print(msg)
+
+        cmd = [ self.path(), "shell", "input", "keyevent", '4' ]
+        subprocess.run(cmd)
+
+        if wait > 0:
+            self.wait(wait)
+
+class pzd:
+
+    def __init__(self):
+        self.adb = ADB()
+        pass
+
+    def ADBPath(self):
+        return self.adb.path()
+
+    def isAvailable(self):
+        return os.path.exists(self.adb.path())
+
+    # タップ
+    def tap(self, x, y, *, msg="", duration=200, wait=0):
+        self.adb.tap(x, y, msg=msg, duration=duration, wait=wait)
+
+    # スワイプ
+    def swipe(self, x1, y1, x2, y2,*, msg="", duration = 0, wait=0):
+        self.adb.swipe(x1, y1, x2, y2, msg=msg, duration=duration, wait=wait)
+
+    # 待つ
+    def wait(self, sec, msg = ""):
+        self.adb.wait(sec, msg=msg)
+
+    # 戻るキー押下
+    def backKey(self,*,msg = "", wait=0):
+        self.adb.backKey(msg=msg, wait=wait)
+
+    # メンバーのスキル使用
+    def useSkill(self, index, *, msg="", wait=0):
+        self.adb.tap(100+index*180, 1155, msg=msg, wait=wait)
+
+    # ダンジョン選択(1階層目)
+    def selectDangeon1(self, index, msg=""):
+        self.adb.tap(600, 580 + index * 280, msg=msg, wait=1)
+    
+    # ダンジョン選択(2階層目)
+    def selectDangeon2(self,index, msg=""):
+        self.adb.tap(600, 580 + index * 280, msg=msg, wait=1)
+
+    # スタミナ回復
+    def kaifuku(self):
+
+        # スタミナ足りません画面かどうかにかかわらずとりあえず押す
+        # (助っ人選択画面だった場合は、行間なので何も起こらない)
+        self.adb.tap(532, 1065, msg="魔法石で回復するボタンを押す", wait=1)
+
+        # ダミー処理
+        # (スタミナが足りていて助っ人選択画面になってた場合に自助っ人を選択)
+        self.adb.tap(500, 570)
+    
+        # スタミナが足りていた場合、この時点で潜入確認画面になっていて何も起こらない
+        self.adb.tap(394, 1353, msg="回復するボタンを押す", wait=2)
+
+        # スタミナ回復を実施した場合は回復しましたボタンをスキップ
+        # 回復をせず、潜入確認画面に進んでいた場合は助っ人選択に戻る
+        # (どちらにせよ、次の状態は助っ人選択画面になる)
+        self.adb.backKey(msg="OKボタンをスキップ", wait=1)
+
+    # 助っ人を選択
+    def selectFriend(self, index):
+
+        # 助っ人を選択
+        self.adb.tap(520, 525 + index * 182, msg=f'助っ人を選択', wait=0.3)
+    
+        # 確定
+        self.adb.tap(424, 1097, wait=1)
+    
+        # 潜入確認
+        self.adb.tap(520, 1757, msg="潜入確認→1Fが始まるまで待機",wait=12)
+    
+    # Tips画面をスキップする
+    def skipTips(self):
+        self.adb.wait(8, msg="TIPS画面がでるまで待機")
+        self.adb.backKey(msg="OKボタンをクリックし待機", wait=10)
+    
+    # 結果画面をスキップ
+    def skipResult(self):
+        self.adb.tap(600, 1582, duration=1900, msg="結果をスキップ", wait=3)
+    
+        self.adb.tap(600, 1416, msg="レベルアップがあったときのエフェクトまち", wait=3)
+        self.adb.tap(600, 1416)
+    
+        self.adb.tap(600, 1416, msg="売却しないのでスキップ")
+
